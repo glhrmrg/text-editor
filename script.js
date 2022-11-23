eb94a9524ada4df9acd5ec29e8f605bbc0b2d4c7
Guilherme <gmgarcia@inf.ufsm.br>
2022-11-23 14:53:51 -0300

    array para percorrer estilos

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const left = document.getElementById('left');
 const center = document.getElementById('center');
 const html = document.querySelector('html');
 const darkmode = document.querySelector('#checkbox');
+let styles = [];
 
 darkmode.addEventListener('change', function(){
     html.classList.toggle('dark');
@@ -17,52 +18,11 @@ send.onclick = function printText () {
     const list = document.createElement("li");
     const insertlist = document.createTextNode(typedText);
     list.appendChild(insertlist);
+    styles.forEach((style) => {
+        list.classList.add(style)
+    });
+    
     field.value = "";
-    
-    if(textarea.style.fontWeight == "bold") {
-        list.setAttribute("class", "bold"); 
-    } 
-    
-    if (textarea.style.fontStyle == "italic") {
-        list.setAttribute("class", "italic"); 
-    }
-    
-    if (textarea.style.fontStyle == "italic" && textarea.style.fontWeight == "bold") {
-        list.setAttribute("class", "italic bold" ); 
-    }
-
-    if (textarea.style.textAlign == "right") {
-        list.setAttribute("class", "rightText"); 
-    }
-
-    if (textarea.style.textAlign == "right" && textarea.style.fontWeight == "bold") {
-        list.setAttribute("class", "rightText bold"); 
-    }
-
-    if (textarea.style.textAlign == "right" && textarea.style.fontStyle == "italic") {
-        list.setAttribute("class", "rightText italic"); 
-    }
-
-    if (textarea.style.textAlign == "right" && textarea.style.fontWeight == "bold" && textarea.style.fontStyle == "italic") {
-        list.setAttribute("class", "rightText bold italic"); 
-    }
-
-    if (textarea.style.textAlign == "center") {
-        list.setAttribute("class", "centerText"); 
-    }
-
-    if (textarea.style.textAlign == "center" && textarea.style.fontWeight == "bold") {
-        list.setAttribute("class", "centerText bold"); 
-    }
-
-    if (textarea.style.textAlign == "center" && textarea.style.fontStyle == "italic") {
-        list.setAttribute("class", "centerText italic"); 
-    }
-
-    if (textarea.style.textAlign == "center" && textarea.style.fontWeight == "bold" && textarea.style.fontStyle == "italic") {
-        list.setAttribute("class", "centerText bold italic"); 
-    }
-
     document.getElementById("textList").appendChild(list);
 }
 
@@ -71,44 +31,97 @@ clear.onclick = function removeText () {
     remove.removeChild(remove.lastElementChild)
 }
 
-bold.onclick = function bold() {
-    if (document.getElementById("textarea").style.fontWeight == "bold") {
-        document.getElementById("textarea").style.fontWeight = "normal";
-    }else {
-        document.getElementById("textarea").style.fontWeight = "bold";
+function addStyle (style) 
+{
+    if (styles.includes(style)) 
+    {
+        styles = styles.filter((styledNow) => styledNow !== style);
+    } else {
+        styles.push(style);
     }
 }
 
-italic.onclick = function italic() {
-    if (document.getElementById("textarea").style.fontStyle == "italic") {
-        document.getElementById("textarea").style.fontStyle = "normal";
-    }else {
-        document.getElementById("textarea").style.fontStyle = "italic";
-    }
-}
+// bold.onclick = function bold() {
+//     if (document.getElementById("textarea").style.fontWeight == "bold") {
+//         document.getElementById("textarea").style.fontWeight = "normal";
+//     }else {
+//         document.getElementById("textarea").style.fontWeight = "bold";
+//     }
+// }
 
-center.onclick = function center() {
-    if (document.getElementById("textarea").style.textAlign == "center") {
-        document.getElementById("textarea").style.textAlign = "left";
-    }else {
-        document.getElementById("textarea").style.textAlign = "center";
-    }
-}
+// italic.onclick = function italic() {
+//     if (document.getElementById("textarea").style.fontStyle == "italic") {
+//         document.getElementById("textarea").style.fontStyle = "normal";
+//     }else {
+//         document.getElementById("textarea").style.fontStyle = "italic";
+//     }
+// }
 
-right.onclick = function right() {
-    if (document.getElementById("textarea").style.textAlign == "right") {
-        document.getElementById("textarea").style.textAlign = "left";
-    }else {
-        document.getElementById("textarea").style.textAlign = "right";
-    }
-}
+// center.onclick = function center() {
+//     if (document.getElementById("textarea").style.textAlign == "center") {
+//         document.getElementById("textarea").style.textAlign = "left";
+//     }else {
+//         document.getElementById("textarea").style.textAlign = "center";
+//     }
+// }
 
-left.onclick = function left() {
-    if (document.getElementById("textarea").style.textAlign == "left") {
-        document.getElementById("textarea").style.textAlign = "left";
-    }else {
-        document.getElementById("textarea").style.textAlign = "left";
-    }
-}
+// right.onclick = function right() {
+//     if (document.getElementById("textarea").style.textAlign == "right") {
+//         document.getElementById("textarea").style.textAlign = "left";
+//     }else {
+//         document.getElementById("textarea").style.textAlign = "right";
+//     }
+// }
+
+// left.onclick = function left() {
+//     if (document.getElementById("textarea").style.textAlign == "left") {
+//         document.getElementById("textarea").style.textAlign = "left";
+//     }else {
+//         document.getElementById("textarea").style.textAlign = "left";
+//     }
+// }
 
 
+// if(textarea.style.fontWeight == "bold") {
+    //     list.setAttribute("class", "bold"); 
+    // } 
+    
+    // if (textarea.style.fontStyle == "italic") {
+    //     list.setAttribute("class", "italic"); 
+    // }
+    
+    // if (textarea.style.fontStyle == "italic" && textarea.style.fontWeight == "bold") {
+    //     list.setAttribute("class", "italic bold" ); 
+    // }
+
+    // if (textarea.style.textAlign == "right") {
+    //     list.setAttribute("class", "rightText"); 
+    // }
+
+    // if (textarea.style.textAlign == "right" && textarea.style.fontWeight == "bold") {
+    //     list.setAttribute("class", "rightText bold"); 
+    // }
+
+    // if (textarea.style.textAlign == "right" && textarea.style.fontStyle == "italic") {
+    //     list.setAttribute("class", "rightText italic"); 
+    // }
+
+    // if (textarea.style.textAlign == "right" && textarea.style.fontWeight == "bold" && textarea.style.fontStyle == "italic") {
+    //     list.setAttribute("class", "rightText bold italic"); 
+    // }
+
+    // if (textarea.style.textAlign == "center") {
+    //     list.setAttribute("class", "centerText"); 
+    // }
+
+    // if (textarea.style.textAlign == "center" && textarea.style.fontWeight == "bold") {
+    //     list.setAttribute("class", "centerText bold"); 
+    // }
+
+    // if (textarea.style.textAlign == "center" && textarea.style.fontStyle == "italic") {
+    //     list.setAttribute("class", "centerText italic"); 
+    // }
+
+    // if (textarea.style.textAlign == "center" && textarea.style.fontWeight == "bold" && textarea.style.fontStyle == "italic") {
+    //     list.setAttribute("class", "centerText bold italic"); 
+    // }
